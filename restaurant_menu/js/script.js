a77d366ebd1b0ce24a7d1b7c0feedcd19539db2d
Ally Elvis Nzeyimana <30961004-allyelvis6569@users.noreply.replit.com>
2024-08-08 14:30:32 +0000

    Update

--- restaurant_menu/js/script.js
+++ restaurant_menu/js/script.js
@@ -1,3 +1,11 @@
+/*
+ * aenzbi - Business Management Software
+ * 
+ * Copyright (c) 2024 Ally Elvis Nzeyimana
+ * 
+ * This software is licensed under the MIT License. See the LICENSE file for details.
+ */
+
 document.addEventListener('DOMContentLoaded', () => {
     const cartItems = [];
     const cartContainer = document.getElementById('cart-items');
--- restaurant_menu/setup_menu.sh
+++ restaurant_menu/setup_menu.sh
@@ -1,3 +1,11 @@
+/*
+ * aenzbi - Business Management Software
+ * 
+ * Copyright (c) 2024 Ally Elvis Nzeyimana
+ * 
+ * This software is licensed under the MIT License. See the LICENSE file for details.
+ */
+
 #!/bin/bash
 
 # Set up directories and files
